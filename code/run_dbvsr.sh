d6ef2788812ac544218d09515c024bf4bf949af0
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-21 13:33:14 +0300

    CLean the repository

--- code/run_dbvsr.sh
+++
@@ -1,3 +0,0 @@
-python3 ../../../code/write_time.py
-python3 main.py --test_only
-python3 ../../../code/write_time.py
--- run.sh
+++
@@ -1,3 +0,0 @@
-mkdir /model/result
-python3 /model/code/main.py --test_only
-chmod -R 0777 /model
